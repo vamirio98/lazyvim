e348d70f4599e117cf68e28fccd5a672544e83dc
vamirio <vamirio@qq.com>
2024-08-18 17:06:10 +0800

    update plugins

--- lua/plugins/lsp.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  opts = {
-    install_root_dir = vim.g.lazyvim_data_dir .. (LazyVim.is_win() and "\\" or "/") .. "mason",
-  },
-}
--- lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    parser_install_dir = vim.g.lazyvim_data_dir .. "/treesitter",
-  },
-  init = function()
-    vim.opt.runtimepath:prepend(vim.g.lazyvim_data_dir .. "/treesitter")
-  end,
-}
